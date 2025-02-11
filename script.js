871727f4fa7eb9d23e362d5a1a671f8548755ee5
Antonix487 <antoniolaurogrotto@gmail.com>
2025-02-11 23:06:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+window.OneSignal = window.OneSignal || [];
 OneSignal.push(function() {
   OneSignal.init({
-    appId: "83c48a06-08e6-47f3-bba6-980e8490aac0", // Inserisci il tuo appId qui
+    appId: "83c48a06-08e6-47f3-bba6-980e8490aac0",  // Sostituisci con il tuo appId corretto
   });
+});
+
 
   // Impostare il pulsante per consentire le notifiche
   document.getElementById("notifyBtn").addEventListener("click", function() {
